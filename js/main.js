002227351ac168a86e9c8a1430e4ff08b10ef5bc
carriesampson <i.carrie.sampson@gmail.com>
2017-10-12 13:51:50 -0400

    created flipCard and checkMatch functions with logs/alertss

--- js/main.js
+++ js/main.js
@@ -1,19 +1,24 @@
 
-var cards = ["queen", "king", "queen", "king"];
+var cards = ["queen", "queen", "king", "king"];
 var cardsInPlay = [];
 
-var cardOne = cards[0];
-cardsInPlay.push("cardOne");
-
-var cardTwo = cards[1];
-cardsInPlay.push("cardTwo");
-
-console.log("User flipped " + cardOne)
-console.log("User flipped " + cardTwo)
-
-if (cardsInPlay.length === 2 && cardsInPlay[0] === cardsInPlay[1]) {
-  alert("You found a match!");
-} else {
+function checkForMatch() {
+  if (cardsInPlay[0] === cardsInPlay[1]) {
+    alert("You found a match!");
+  } else {
     alert("Sorry, try again.");
   }
+}
+
+function flipCard(cardID) {
+  console.log("User flipped " + cards[cardID]);
+  cardsInPlay.push(cards[cardID]);
+}
+
+flipCard(0);
+flipCard(2);
+checkForMatch();
+
+
+
 
