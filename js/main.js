a78454a6c5c30f5710efa36836022d73f26d3499
carriesampson <i.carrie.sampson@gmail.com>
2017-10-11 20:25:15 -0400

    created comparison logic

--- js/main.js
+++ js/main.js
@@ -1,9 +1,19 @@
-console.log("Up and running!");
 
-var cardOne = "queen";
-var cardTwo = "queen";
-var cardThree = "king";
-var cardFour = "king";
+var cards = ["queen", "king", "queen", "king"];
+var cardsInPlay = [];
 
-console.log("User flipped " + cardOne);
-console.log("User flipped " + cardThree);
+var cardOne = cards[0];
+cardsInPlay.push("cardOne");
+
+var cardTwo = cards[1];
+cardsInPlay.push("cardTwo");
+
+console.log("User flipped " + cardOne)
+console.log("User flipped " + cardTwo)
+
+if (cardsInPlay.length === 2 && cardsInPlay[0] === cardsInPlay[1]) {
+  alert("You found a match!");
+} else {
+    alert("Sorry, try again.");
+  }
+
